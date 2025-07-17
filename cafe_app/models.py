80e5f2be707364e82bc927fad6697f6f31eb3bd7
FeliciaIonela24 <felicia_ciobotaru@outlook.com>
2025-07-17 12:24:33 +0300

    update comments

--- cafe_app/models.py
+++ cafe_app/models.py
@@ -1,5 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy #allows working with relational databases in Flask (using ORM – Object Relational Mapper).
+from flask_login import UserMixin #useful methods for authentication (e.g. is_authenticated, is_active, etc.). Used for the User class.
 
 db = SQLAlchemy()
 
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(150), nullable=False)
     
      # Relationship with the cart(cart items)
-    cart_items = db.relationship('CartItem', backref='user', lazy=True)
+    cart_items = db.relationship('CartItem', backref='user', lazy=True) #lazy=True: data will only be loaded when accessed
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,4 +47,4 @@ class Product(db.Model):
 class CartItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False) #links the product in the cart to a user, nullable=False = must belong to someone.
--- create_admin.py
+++ create_admin.py
@@ -1,11 +1,12 @@
-from cafe_app import create_app, db
+from cafe_app import create_app, db 
+#Imports the create_app() function that configures the Flask application and the db (database) object from the cafe_app package.
 from cafe_app.models import User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash #Imports the function that encrypts the password to store it securely in the database.
 
 app = create_app()
 
-with app.app_context():
-    if not User.query.filter_by(username='admin').first():
+with app.app_context():#database interaction outside of a real request
+    if not User.query.filter_by(username='admin').first(): #create user admin if it does not exist
         admin = User(username='admin', password=generate_password_hash('admin123'))
         db.session.add(admin)
         db.session.commit()
